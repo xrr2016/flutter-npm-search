f2e4292dd1eb1547c60979fd1bcfcc5006bd7161
xrr2016 <xiaoranran1993@163.com>
2017-05-04 23:22:17 +0800

    :boom:

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-# react_curd
-
-用react实现简单的数据CURD操作
+NOT Thing Here!
--- client/src/reducers/rootReducer.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux'
-
-import games from './game'
-
-export default combineReducers({
-  games
-})
